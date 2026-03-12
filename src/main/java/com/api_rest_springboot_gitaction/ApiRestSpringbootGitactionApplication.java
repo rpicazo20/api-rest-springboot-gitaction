fc439613c3f1aad97a09727a6efcdb5c9db9ae2b
rpicazo20 <rpicazo.techfactory@gmail.com>
2026-03-12 10:53:19 -0600

    primer commit

---
+++ src/main/java/com/api_rest_springboot_gitaction/ApiRestSpringbootGitactionApplication.java
@@ -0,0 +1,13 @@
+package com.api_rest_springboot_gitaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSpringbootGitactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSpringbootGitactionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api_rest_springboot_gitaction/ApiRestSpringbootGitactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_rest_springboot_gitaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringbootGitactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
